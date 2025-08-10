6a5bfc1695e60e2848eaf50886bd700f66a1321d
Sebastian Cunningham <80603287+sebcun@users.noreply.github.com>
2025-08-10 13:57:20 +1000

    FAQ & Home
    Changed to templates, as well as added FAQ and improved home page.

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/faq')
+def faq():
+    return render_template('faq.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
